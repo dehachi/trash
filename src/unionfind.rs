666da1d21770268b303420a3fc6756a225afae7f
dehachi <59132212+dehachi@users.noreply.github.com>
2021-01-31 23:04:16 +0900

    Add files via upload

---
+++ src/unionfind.rs
@@ -0,0 +1,35 @@
+struct UnionFind {
+    par: Vec<isize>
+}
+
+impl UnionFind {
+    fn init(n: usize) -> UnionFind {
+        return UnionFind{par: vec![-1; n]};
+    }
+    fn root(&mut self, x: usize) -> usize{
+        if self.par[x] < 0 {
+            return x;
+        } else {
+            self.par[x] = self.root(self.par[x] as usize) as isize;
+            return self.par[x] as usize
+        }
+    }
+    fn same(&mut self, x: usize, y: usize) -> bool {
+        return self.root(x) == self.root(y);
+    }
+    fn merge(&mut self, x: usize, y: usize) -> bool{
+        let mut rx = self.root(x);
+        let mut ry = self.root(y);
+        if rx == ry {return false;};
+        if self.par[rx] > self.par[ry] {
+            std::mem::swap(&mut rx, &mut ry);
+        }
+        self.par[rx] += self.par[ry];
+        self.par[ry] = rx as isize;
+        return true;
+    }
+    fn size(&mut self, x: usize) -> usize {
+        let p = self.root(x);
+        return -self.par[p] as usize;
+    }
+}
