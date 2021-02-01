1cf0c13065655a6b81ab98075e8bea60f0fbfad6
dehachi <59132212+dehachi@users.noreply.github.com>
2021-02-02 00:37:58 +0900

    swap of name (length <-> n)

--- src/Salesman.rs
+++ src/Salesman.rs
@@ -1,19 +1,19 @@
 struct Salesman {
     dp: Vec<Vec<isize>>,
-    length: usize,
+    n: usize,
     d: Vec<Vec<usize>>
 }
 
 impl Salesman {
-    fn init(n: usize, cost: Vec<Vec<usize>>) -> Salesman {
-        let table = vec![vec![-1; n]; 1<<n];
-        return Salesman{dp: table, length: n, d: cost};
+    fn init(length: usize, cost: Vec<Vec<usize>>) -> Salesman {
+        let table = vec![vec![-1; length]; 1<<length];
+        return Salesman{dp: table, n: length, d: cost};
     }
     fn distance(&mut self, s: usize, v: usize) -> usize{
         if self.dp[s][v] != -1 {return self.dp[s][v] as usize;}
-        if s == (1<<(self.length))-1 && v==0 {return 0;}
+        if s == (1<<(self.n))-1 && v==0 {return 0;}
         let mut res = std::i32::MAX as usize;
-        for u in 0..self.length {
+        for u in 0..self.n {
             if s>>u & 1 == 0 {
                 res = res.min(self.distance(s | (1<<u), u) + self.d[v][u]);
             }
