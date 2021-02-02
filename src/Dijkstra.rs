d0ccd8a7d4e8d87af072d2aca272ee69174f275d
dehachi <59132212+dehachi@users.noreply.github.com>
2021-02-02 15:58:52 +0900

    add f(path)

--- src/Dijkstra.rs
+++ src/Dijkstra.rs
@@ -1,13 +1,15 @@
 struct Dijkstra {
     graph: Vec<Vec<(usize, usize)>>,
     n: usize,
-    dist: Vec<usize>
+    dist: Vec<usize>,
+    prev: Vec<isize>
 }
 
 impl Dijkstra {
     fn init(length: usize, g: Vec<Vec<(usize, usize)>>) -> Dijkstra {
-        let d = vec![std::u32::MAX as usize; length+5];
-        return Dijkstra{graph: g, n: length, dist: d}
+        let d = vec![std::u32::MAX as usize; length];
+        let p = vec![-1; length];
+        return Dijkstra{graph: g, n: length, dist: d, prev: p}
     }
     fn calc(&mut self, s:usize) {
         self.dist[s] = 0;
@@ -17,9 +19,10 @@ impl Dijkstra {
             let (d, pos) = heap.pop().unwrap();
             if d > self.dist[pos] {continue;}
             for (to, cost) in self.graph[pos].iter() {
-                let next = &self.dist[pos] + cost;
+                let next = self.dist[pos] + cost;
                 if next < self.dist[*to] {
                     self.dist[*to] = next;
+                    self.prev[*to] = pos as isize;
                     heap.push((next, *to));
                 }
             }
@@ -27,5 +30,15 @@ impl Dijkstra {
     }
     fn distance(&mut self, v: usize) -> usize{
         return self.dist[v];
+    }
+    fn path(&mut self, v: usize) -> Vec<usize> {
+        let mut p = vec![];
+        let mut i = v as isize;
+        while i != -1 {
+            p.push(i as usize);
+            i = self.prev[i as usize] as isize;
+        }
+        p.reverse();
+        return p;
     }
 }
