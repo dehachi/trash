477502c5a83391fdcb9d1a6c567ac0b222255dce
dehachi <59132212+dehachi@users.noreply.github.com>
2021-02-10 00:03:08 +0900

    saidaihi-pujan

--- src/Dijkstra.rs
+++ src/Dijkstra.rs
@@ -1,44 +1,34 @@
-struct Dijkstra {
-    graph: Vec<Vec<(usize, usize)>>,
-    n: usize,
-    dist: Vec<usize>,
-    prev: Vec<isize>
-}
-
-impl Dijkstra {
-    fn init(length: usize, g: Vec<Vec<(usize, usize)>>) -> Dijkstra {
-        let d = vec![std::u32::MAX as usize; length];
-        return Dijkstra{graph: g, n: length, dist: d, prev: vec![-1; length]}
-    }
-    fn clean(&mut self) {
-        self.dist = vec![std::u32::MAX as usize; ];
-    }
-    fn calc(&mut self, s:usize) {
-        self.dist[s] = 0;
+mod Dijkstra {
+    pub fn distance(n: usize, graph: &Vec<Vec<(usize, isize)>>, s: usize, mode: u8) -> Vec<isize> {
+        let mut dist = vec![std::u32::MAX as isize; n];
+        let mut prev = vec![-1; n];
+        dist[s] = 0;
         let mut heap = std::collections::BinaryHeap::new();
         heap.push((0, s));
         while !heap.is_empty() {
             let (d, pos) = heap.pop().unwrap();
-            if d > self.dist[pos] {continue;}
-            for (to, cost) in self.graph[pos].iter() {
-                let next = self.dist[pos] + cost;
-                if next < self.dist[*to] {
-                    self.dist[*to] = next;
-                    self.prev[*to] = pos as isize;
-                    heap.push((next, *to));
+            if d > dist[pos] {continue;}
+            for (to, cost) in graph[pos].iter() {
+                let next = dist[pos] + cost;
+                if next < dist[*to] {
+                    dist[*to] = next;
+                    prev[*to] = pos as isize;
+                    heap.push((-next, *to));
                 }
             }
         }
+        if mode == 0 {
+            return dist;
+        } else {
+            return prev;
+        }
     }
-    fn distance(&mut self, v: usize) -> usize{
-        return self.dist[v];
-    }
-    fn path(&mut self, v: usize) -> Vec<usize> {
+    pub fn path(v: usize, prev: Vec<isize>) -> Vec<usize> {
         let mut p = vec![];
         let mut i = v as isize;
         while i != -1 {
             p.push(i as usize);
-            i = self.prev[i as usize] as isize;
+            i = prev[i as usize] as isize;
         }
         p.reverse();
         return p;
