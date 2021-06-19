fe5fdb6541b65476dfbc8a9c1071d263345aedf0
dehachi <59132212+dehachi@users.noreply.github.com>
2021-06-20 00:48:14 +0900

    Update Counter.rs

--- src/Counter.rs
+++ src/Counter.rs
@@ -1,5 +1,5 @@
-fn Counter<T: Eq + Hash>(v: Vec<T>) -> HashMap<T, usize> {
-    let mut hm = HashMap::new();
+fn Counter<T: Eq + std::hash::Hash>(v: Vec<T>) -> std::collections::HashMap<T, usize> {
+    let mut hm = std::collections::HashMap::new();
     for i in v {
         let cnt = hm.entry(i).or_insert(0);
         *cnt += 1;
