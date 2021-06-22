7e3d12163804d657b8a2cdd69cdcf461d783170f
dehachi <59132212+dehachi@users.noreply.github.com>
2021-06-23 00:30:32 +0900

    Update Ketawa.rs

--- src/Ketawa.rs
+++ src/Ketawa.rs
@@ -1,5 +1,6 @@
-fn Ketawa(mut n: usize) -> usize {
+fn Ketawa(mut n: &usize) -> usize {
     let mut ret = 0;
+    let mut n = n.clone();
     while n > 0 {
         ret += n % 10;
         n /= 10;
