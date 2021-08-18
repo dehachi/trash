2af5826f5e81f9d1b194599813a0405d4f46cc31
dehachi <59132212+dehachi@users.noreply.github.com>
2021-08-19 05:03:05 +0900

    Update Dijkstra.cpp

--- cpp/Dijkstra.cpp
+++ cpp/Dijkstra.cpp
@@ -1,7 +1,7 @@
 /*
 ダイクストラ法
 
-頂点sからの単一視点最短経路: Dijkstra(頂点s, 隣接リスト{頂点, コスト})
+頂点sからの単一始点最短経路: Dijkstra(頂点s, 隣接リスト{頂点, コスト})
 
 verified: https://judge.u-aizu.ac.jp/onlinejudge/review.jsp?rid=5801897#1
 */
