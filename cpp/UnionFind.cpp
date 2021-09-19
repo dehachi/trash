7535b80b553a357de8ceae516d397ada8bd8effc
dehachi <59132212+dehachi@users.noreply.github.com>
2021-09-19 18:11:38 +0900

    Update UnionFind.cpp

--- cpp/UnionFind.cpp
+++ cpp/UnionFind.cpp
@@ -13,38 +13,37 @@ verified: https://judge.yosupo.jp/submission/56611
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-
 struct UnionFind {
-    int flen;
-    vector<int> par;
-    UnionFind(int n) {init(n);}
-    void init(int n) {
-        par.assign(n, -1);
-        flen = n;
-    }
-    int root(int x) {
-        if (par[x] < 0) return x;
-        return par[x] = root(par[x]);
-    }
-    bool merge(int x, int y) {
-        x = root(x);
-        y = root(y);
-        if (x == y) return false;
-        if (par[x] > par[y]) swap(x, y);
-        par[x] += par[y];
-        par[y] = x;
-        flen--;
-        return true;
-    }
-    bool same(int x, int y) {
-        return root(x) == root(y);
-    }
-    int size(int x) {
-        return -par[root(x)];
-    }
-    int forest_size() {
-        return flen;
-    }
+	int flen;
+	vector<int> par;
+	UnionFind(int n) {init(n);}
+	void init(int n) {
+		par.assign(n, -1);
+		flen = n;
+	}
+	int root(int x) {
+		if (par[x] < 0) return x;
+		return par[x] = root(par[x]);
+	}
+	bool merge(int x, int y) {
+		x = root(x);
+		y = root(y);
+		if (x == y) return false;
+		if (par[x] > par[y]) swap(x, y);
+		par[x] += par[y];
+		par[y] = x;
+		flen--;
+		return true;
+	}
+	bool same(int x, int y) {
+		return root(x) == root(y);
+	}
+	int size(int x) {
+		return -par[root(x)];
+	}
+	int forest_size() {
+		return flen;
+	}
 };
 
 int main() {
