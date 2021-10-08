daed2d0a2704b3b987ea9345010f02700cd93370
dehachi <59132212+dehachi@users.noreply.github.com>
2021-10-09 00:55:49 +0900

    Update 素因数列挙.cpp

--- cpp/素因数列挙.cpp
+++ cpp/素因数列挙.cpp
@@ -1,12 +1,19 @@
-vector<ll> prime_enumerate(ll N) {
-    vector<ll> res;
-    for (ll i=2; i+i<=N; i++) {
+/*
+素因数列挙
+
+nの素因数を昇順に列挙: prime_enumerate(n)
+*/
+
+template <class T>
+vector<T> prime_enumerate(T N) {
+    vector<T> res;
+    for (T i=2; i+i<=N; i++) {
         if (N % i != 0) continue;
         while (N % i == 0) {
             N /= i;
         }
         res.push_back(i);
     }
-    if (N != 1) res.push_back(1);
+    if (N != 1) res.push_back(N);
     return res;
 }
