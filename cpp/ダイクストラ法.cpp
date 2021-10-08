ff14756afd573651cd606d1077b357d7d2612cb0
dehachi <59132212+dehachi@users.noreply.github.com>
2021-10-08 21:10:01 +0900

    Update ダイクストラ法.cpp

--- cpp/ダイクストラ法.cpp
+++ cpp/ダイクストラ法.cpp
@@ -4,14 +4,17 @@
 verified: https://judge.u-aizu.ac.jp/onlinejudge/review.jsp?rid=5801897#1
 */
 
+#pragma GCC optimize("O3")
 #include <bits/stdc++.h>
+#define ll long long
+#define ALL(V) V.begin(), V.end()
 using namespace std;
-typedef long long ll;
 
-vector<int> dijkstra(int s, vector<vector<pair<int, int>>>& g) {
-	vector<int> dis(g.size(), INT32_MAX);
+template <class T>
+vector<T> dijkstra(int s, vector<vector<pair<int, T>>>& g) {
+	vector<T> dis(g.size(), INT32_MAX);
 	dis[s] = 0;
-	typedef pair<int,int> P;
+	typedef pair<int,T> P;
 	priority_queue<P, vector<P>, greater<P>> heap;
 	heap.push({0, s});
 	while (!heap.empty()) {
@@ -29,20 +32,21 @@ vector<int> dijkstra(int s, vector<vector<pair<int, int>>>& g) {
 	return dis;
 }
 
-
 int main() {
-    int v, e, r;
-    cin >> v >> e >> r;
-    vector<vector<pair<int,int>>> g(v);
-    while (e--) {
-        int s, t, d;
-        cin >> s >> t >> d;
-        g[s].push_back({t, d});
-    }
-    for (int i: Dijkstra(r, g)) {
-        if (i == INT32_MAX) cout << "INF";
-        else cout << i;
-        cout << "\n";
-    }
-    return 0;
+	cin.tie(0);
+	ios::sync_with_stdio(0);
+	int v, e, r;
+	cin >> v >> e >> r;
+	vector<vector<pair<int,int>>> g(v);
+	while (e--) {
+		int s, t, d;
+		cin >> s >> t >> d;
+		g[s].push_back({t, d});
+	}
+	for (int i: dijkstra(r, g)) {
+		if (i == INT32_MAX) cout << "INF";
+		else cout << i;
+		cout << "\n";
+	}
+	return 0;
 }
