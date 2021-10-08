9c8b2f80890c92203e2d9f3c657b2cbddbd2d200
dehachi <59132212+dehachi@users.noreply.github.com>
2021-10-09 00:50:12 +0900

    Update 約数列挙.cpp

--- cpp/約数列挙.cpp
+++ cpp/約数列挙.cpp
@@ -4,9 +4,10 @@
 nの約数を昇順に列挙する: divisor_enumerate(n)
 */
 
-vector<ll> divisor_enumerate(ll N) {
-    vector<ll> res;
-    for (ll i=1; i*i<=N; i++) {
+template <class T>
+vector<T> divisor_enumerate(T N) {
+    vector<T> res;
+    for (T i=1; i*i<=N; i++) {
         if (N % i == 0) {
             res.push_back(i);
             if (N/i != i) res.push_back(N/i);
