15c06bb7527d38dd7c6c09c538b2a77bef3f1a27
dehachi <59132212+dehachi@users.noreply.github.com>
2021-10-09 01:00:53 +0900

    Update 素因数分解.cpp

--- cpp/素因数分解.cpp
+++ cpp/素因数分解.cpp
@@ -4,6 +4,10 @@
 nを(素因数, 指数) の形で素因数分解(素因数は昇順): prime_factorize(n)
 */
 
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
 template <class T>
 vector<pair<T,int>> prime_factorize(T N) {
     vector<pair<ll,int>> res;
