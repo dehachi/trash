9d9355dcb68fd42246f31a742037f14df0a7c82b
dehachi <59132212+dehachi@users.noreply.github.com>
2021-10-09 00:57:40 +0900

    Update 素数判定.cpp

--- cpp/素数判定.cpp
+++ cpp/素数判定.cpp
@@ -1,6 +1,13 @@
-bool is_prime(ll N) {
+/*
+素数判定
+
+nの素数判定: is_prime(n)
+*/
+
+template <class T>
+bool is_prime(T N) {
     if (N == 1) return false;
-    for (ll i=2; i*i<=N; i++) {
+    for (T i=2; i*i<=N; i++) {
         if (N % i == 0) return false;
     }
     return true;
