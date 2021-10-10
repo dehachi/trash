6013bc8ebe758cadbcad915092f895dd6258cf15
dehachi <59132212+dehachi@users.noreply.github.com>
2021-10-10 20:30:11 +0900

    Create 深さ優先探索(最短経路パス).cpp

---
+++ cpp/深さ優先探索(最短経路パス).cpp
@@ -0,0 +1,26 @@
+struct Dfs_ShortestPath {
+	int s, n;
+	vector<int> prev;
+	vector<vector<int>> g;
+	Dfs_ShortestPath(int s, vector<vector<int>>& g)
+		: s(s), g(g), n(g.size()), prev(n, -1) {build();}
+	void build() {
+		auto f = [&](auto self, int pos, int par) -> void {
+			prev[pos] = par;
+			for (int to: g[pos]) if (to != par) {
+				self(self, to, pos);
+			}
+		};
+		f(f, s, -1);
+	}
+	vector<int> path(int v) {
+		vector<int> p;
+		int i = v;
+		while (i != -1) {
+			p.push_back(i);
+			i = prev[i];
+		}
+		reverse(p.begin(), p.end());
+		return p;
+	}
+};
