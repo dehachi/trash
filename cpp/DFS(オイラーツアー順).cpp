6f3ede722b3ce9bf69713aceee4c3e40528098ad
dehachi <59132212+dehachi@users.noreply.github.com>
2021-10-16 21:58:19 +0900

    Create DFS(オイラーツアー順).cpp

---
+++ cpp/DFS(オイラーツアー順).cpp
@@ -0,0 +1,18 @@
+/*
+深さ優先探索オイラーツアーの頂点経路
+
+根をsとしてオイラーツアー: euler_tour_path(s, 隣接リスト)
+*/
+
+vector<int> euler_tour_path(int s, vector<vector<int>>& g) {
+	vector<int> res;
+	auto f = [&](auto self, int pos, int par) -> void {
+		res.push_back(pos);
+		for (int to: g[pos]) if (to != par) {
+			self(self, to, pos);
+			res.push_back(pos);
+		}
+	};
+	f(f, s, -1);
+	return res;
+}
