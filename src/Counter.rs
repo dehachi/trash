f916d910dbf9436794ebe2d2787fe52363ffa3d6
dehachi <59132212+dehachi@users.noreply.github.com>
2022-01-13 23:34:43 +0900

    Delete src directory

--- src/Counter.rs
+++
@@ -1,8 +0,0 @@
-fn Counter<T: Eq + std::hash::Hash>(v: Vec<T>) -> std::collections::HashMap<T, usize> {
-    let mut hm = std::collections::HashMap::new();
-    for i in v {
-        let cnt = hm.entry(i).or_insert(0);
-        *cnt += 1;
-    }
-    return hm;
-}
--- src/Ketawa.rs
+++
@@ -1,9 +0,0 @@
-fn Ketawa(mut n: &usize) -> usize {
-    let mut ret = 0;
-    let mut n = n.clone();
-    while n > 0 {
-        ret += n % 10;
-        n /= 10;
-    }
-    return ret;
-}
