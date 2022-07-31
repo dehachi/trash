9e09f79c826e1ddf0d90c1bf472cb085b9c8664f
dehachi <59132212+dehachi@users.noreply.github.com>
2022-08-01 01:53:32 +0900

    Update Dijkstra法(最短経路長).cpp

--- procon/library/Dijkstra法(最短経路長).cpp
+++ procon/library/Dijkstra法(最短経路長).cpp
@@ -1,21 +1,16 @@
-/*
-Dijkstra法による単一始点最短経路長
-
-頂点sからの単一始点最短経路長: dijkstra_shortest_distance(頂点s, 隣接リスト{頂点, コスト})
-*/
-
-vector<ll> dijkstra_shortest_distance(int s, vector<vector<pair<int, ll>>>& g) {
-	vector<ll> dis(g.size(), LLONG_MAX);
-	dis[s] = 0LL;
-	typedef pair<ll,int> P;
+template <class T>
+vector<T> dijkstra(int s, vector<vector<pair<int,T>>> &g) {
+	vector<int> dis(g.size(), std::numeric_limits<T>::max());
+	typedef pair<T,int> P;
 	priority_queue<P, vector<P>, greater<P>> pq;
-	pq.push({0LL, s});
+	dis[s] = 0;
+	pq.push({0, s});
 	while (!pq.empty()) {
-		auto [v, pos] = pq.top();
+		auto [d, pos] = pq.top();
 		pq.pop();
-		if (v < dis[pos]) continue;
-		for (auto [to, cost]: g[pos]) {
-			if (dis[to] > dis[pos] + cost) {
+		if (d > dis[pos]) continue;
+		for (auto &&[to, cost]: g[pos]) {
+			if (dis[pos]+cost < dis[to]) {
 				dis[to] = dis[pos] + cost;
 				pq.push({dis[to], to});
 			}
