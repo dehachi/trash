72b608d469544bcfca11441d4bcb1e24f2d6e31d
dehachi <59132212+dehachi@users.noreply.github.com>
2022-09-11 18:32:15 +0900

    Update UnionFind.hpp

--- lib/UnionFind.hpp
+++ lib/UnionFind.hpp
@@ -1,32 +1,30 @@
 struct UnionFind {
-	int flen;
-	vector<int> par;
-	UnionFind(int n) {build(n);}
-	void build(int n) {
-		par.assign(n, -1);
-		flen = n;
+	int fsize;
+	vector<int> par, sz;
+	UnionFind(int n): fsize(n), par(n), sz(n, 1) {
+		for (int i=0; i<n; i++) par[i] = i;
 	}
 	int root(int x) {
-		if (par[x] < 0) return x;
-		return par[x] = root(par[x]);
+		if (x != par[x]) par[x] = root(par[x]);
+		return par[x];
 	}
 	bool merge(int x, int y) {
 		x = root(x);
 		y = root(y);
 		if (x == y) return false;
-		if (par[x] > par[y]) swap(x, y);
-		par[x] += par[y];
-		par[y] = x;
-		flen--;
+		if (sz[x] > sz[y]) swap(x, y);
+		par[x] = y;
+		sz[y] += sz[x];
+		fsize--;
 		return true;
 	}
 	bool same(int x, int y) {
 		return root(x) == root(y);
 	}
 	int size(int x) {
-		return -par[root(x)];
+		return sz[root(x)];
 	}
 	int forest_size() {
-		return flen;
+		return fsize;
 	}
 };
