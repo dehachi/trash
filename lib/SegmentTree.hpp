62332f9a5ba1664bb7798e71300541317d04455e
dehachi <59132212+dehachi@users.noreply.github.com>
2022-12-29 03:30:22 +0900

    Update SegmentTree.hpp

--- lib/SegmentTree.hpp
+++ lib/SegmentTree.hpp
@@ -1,8 +1,3 @@
-#pragma GCC optimize("O3")
-#include <bits/stdc++.h>
-#define ll long long
-using namespace std;
-
 template <typename T>
 struct SegmentTree {
 	int n, size;
@@ -29,41 +24,5 @@ struct SegmentTree {
 		}
 		return f(res_l, res_r);
 	}
-	T &operator[](const int &i) {return &dat[i+size];}
+	T &operator[](int i) {return dat[i+size];}
 };
-
-constexpr int INF = 1<<30;
-
-int N;
-int P[2<<17], ans[2<<17];
-
-void calc() {
-	SegmentTree<int> seg(N+1, INF, [](int a, int b){return min(a,b);});
-	SegmentTree<int> seg2(N+1, INF, [](int a, int b){return min(a,b);});
-	for (int i=0; i<N; i++) {
-		int s = seg.fold(0, P[i]), s2 = seg2.fold(P[i], N+1);
-		if (s<INF) ans[i] = min(ans[i], P[i]+i+s);
-		if (s2<INF) ans[i] = min(ans[i], -P[i]+i+s2);
-		seg.set(P[i], -P[i]-i);
-		seg2.set(P[i], P[i]-i);
-	}
-}
-
-void solve() {
-	cin >> N;
-	for (int i=0; i<N; i++) cin >> P[i];
-	fill(ans, ans+N, INF);
-	calc();
-	reverse(P, P+N);
-	reverse(ans, ans+N);
-	calc();
-	for (int i=N-1; i>=0; i--) cout << ans[i] << (!i ? '\n' : ' ');
-}
-
-signed main() {
-	cin.tie(nullptr);
-	ios_base::sync_with_stdio(false);
-	cout << fixed << setprecision(20);
-	solve();
-	return 0;
-}
