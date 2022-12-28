350dcd2b0a305710f9b1c5d4eeb19dbc9a97cfbc
dehachi <59132212+dehachi@users.noreply.github.com>
2022-12-29 01:13:31 +0900

    Update SegmentTree.hpp

--- lib/SegmentTree.hpp
+++ lib/SegmentTree.hpp
@@ -1,25 +1,28 @@
 template <typename T>
 struct SegmentTree {
-	int n;
+	int n, size;
 	vector<T> dat;
 	using F = function<T(T,T)>;
 	F f;
 	T e;
-	SegmentTree(int _n, F _f, T _e): n(_n), dat((n<<1)|1, _e), f(_f), e(_e) {};
+	SegmentTree(int _n, T _e, F _f): n(_n), e(_e), f(_f) {
+		size = 1;
+		while (size < _n) size <<= 1;
+		dat.assign(size<<1, e);
+	};
 	void set(int i, T x) {
-		dat[i+=n] = x;
-		while (i) {
-			i >>= 1;
+		dat[i+=size] = x;
+		while (i >>= 1) {
 			dat[i] = f(dat[i<<1], dat[(i<<1)|1]);
 		}
 	}
 	T fold(int l, int r) {
 		T res_l = e, res_r = e;
-		for (l+=n, r+=n; l<r; l>>=1, r>>=1) {
+		for (l+=size, r+=size; l<r; l>>=1, r>>=1) {
 			if (l&1) res_l = f(res_l, dat[l++]);
 			if (r&1) res_r = f(dat[--r], res_r);
 		}
 		return f(res_l, res_r);
 	}
-	T operator[](const int &i) const {return dat[i+n];}
+	T &operator[](const int &i) {return dat[i+size];}
 };
