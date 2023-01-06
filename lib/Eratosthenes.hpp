f09da5539502c8e9f98672c9324c21ae538f37f9
dehachi <59132212+dehachi@users.noreply.github.com>
2023-01-06 23:51:09 +0900

    Update Eratosthenes.hpp

--- lib/Eratosthenes.hpp
+++ lib/Eratosthenes.hpp
@@ -2,7 +2,7 @@ struct Eratosthenes {
 	int n;
 	vector<bool> table;
 	Eratosthenes(int _n): n(_n), table(_n+1, true) {
-		table[1] = false;
+		table[0] = table[1] = false;
 		for (int i=2; i<=_n; i++) if (table[i]) {
 			for (int j=i*2; j<=_n; j+=i) table[j] = false;
 		}
