b24142ac5d69fb6d1bfe7b6ee3dc660eb5a703a5
dehachi <59132212+dehachi@users.noreply.github.com>
2023-01-26 22:12:19 +0900

    Update CRT.hpp

--- lib/CRT.hpp
+++ lib/CRT.hpp
@@ -1,32 +1,28 @@
 template <typename T>
 struct CRT {
-	vector<T> rem, mod;
+	T rem, mod;
+	CRT(): rem(0), mod(1) {}
 	void add(T r, T m) {
-		rem.emplace_back(r);
-		mod.emplace_back(m);
-	}
-	pair<T,T> calc() {
-		T r = 0, m = 1;
-		for (int i=0; i<(int)rem.size(); i++) {
-			auto ext_gcd = [](T a, T b, T &x, T &y, T &d) -> void {
-				x = 1, y = 0;
-				T u = 0, v = 1;
-				while (b) {
-					T p = a/b, q = a%b, r = x-u*p;
-					x = u, u = r;
-					r = y-v*p;
-					y = v, v = r;
-					a = b, b = q;
-				}
-				d = a;
-			};
-			T p, q, d;
-			ext_gcd(m, mod[i], p, q, d);
-			if ((rem[i]-r)%d != 0) return {0, 0};
-			T t = (rem[i]-r)/d*p%(mod[i]/d);
-			r += m*t;
-			m *= mod[i]/d;
+		auto ext_gcd = [](T a, T b, T &x, T &y, T &d) -> void {
+			x = 1, y = 0;
+			T u = 0, v = 1;
+			while (b) {
+				T p = a/b, q = a%b, r = x-u*p;
+				x = u, u = r;
+				r = y-v*p;
+				y = v, v = r;
+				a = b, b = q;
+			}
+			d = a;
+		};
+		T p, q, d;
+		ext_gcd(mod, m, p, q, d);
+		if ((r-rem)%d != 0) {
+			rem = mod = 0;
+			return;
 		}
-		return {r<0?r+m:r, m};
+		rem += mod*((r-rem)/d*p%(m/d));
+		mod *= m/d;
 	}
+	pair<T,T> get() {return {rem<0?rem+mod:rem, mod};}
 };
